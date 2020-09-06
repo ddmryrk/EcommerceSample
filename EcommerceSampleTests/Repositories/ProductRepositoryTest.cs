9c38cbf7c2982c4cd725fbb0bfe087326c07e75e
ddmryrk <d.demiryurek@windowslive.com>
2020-09-07 01:21:28 +0300

    added new util tests

---
+++ EcommerceSampleTests/Repositories/ProductRepositoryTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using EcommerceSample.Entities;
+using EcommerceSample.Repositories;
+using Xunit;
+using Moq;
+
+
+namespace EcommerceSampleTests.Repositories
+{
+    public class ProductRepositoryTest
+    {
+        public ProductRepositoryTest()
+        {
+        }
+
+        [Fact]
+        public void WhenAddProduct_AddedSuccesfully()
+        {
+            var phoneCategory = new Category("Phone");
+            var product1 = new Product("İphone S5", 1000, phoneCategory);
+            var repository = new ProductRepository();
+
+            var result = repository.Add(product1);
+            var count = repository.GetAll().Count;
+
+            Assert.True(result.IsSucceed);
+            Assert.Equal(1, count);
+        }
+
+        [Fact]
+        public void WhenAddMultipleProduct_AddedSuccesfully()
+        {
+            var phoneCategory = new Category("Phone");
+            var product1 = new Product("İphone S5", 1000, phoneCategory);
+            var product2 = new Product("İphone 6", 1500, phoneCategory);
+            var repository = new ProductRepository();
+
+            var result1 = repository.Add(product1);
+            var result2 = repository.Add(product2);
+
+            Assert.True(result1.IsSucceed);
+            Assert.True(result2.IsSucceed);
+            Assert.Equal(2, repository.GetAll().Count);
+        }
+
+        [Fact]
+        public void WhenAddProduct_IDControlSuccessful()
+        {
+            var phoneCategory = new Category("Phone");
+            var product1 = new Product("İphone S5", 1000, phoneCategory);
+            var repository = new ProductRepository();
+            repository.Add(product1);
+
+            var selectedProduct = repository.GetByID(0);
+
+            Assert.NotNull(selectedProduct);
+        }
+
+        [Fact]
+        public void WhenAddProductIdNotFound_ExpectNull()
+        {
+            var phoneCategory = new Category("Phone");
+            var product1 = new Product("İphone S5", 1000, phoneCategory);
+            var repository = new ProductRepository();
+            repository.Add(product1);
+
+            var selectedProduct = repository.GetByID(20);
+
+            Assert.Null(selectedProduct);
+        }       
+    }
+}
---
+++ EcommerceSampleTests/Util/HelperTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using EcommerceSample.Entities;
+using EcommerceSample.Util;
+using EcommerceSample.Repositories;
+using Xunit;
+
+namespace EcommerceSampleTests.Util
+{
+    public class HelperTest
+    {
+        private List<Category> _listToBeCalculated;
+        private CategoryRepository _repository;
+        public HelperTest()
+        {
+            _listToBeCalculated = new List<Category>()
+            {
+                new Category("1"),
+                new Category("2"),
+                new Category("3"),
+                new Category("4"),
+                new Category("5"),
+                new Category("6")
+            };
+
+            _repository = new CategoryRepository();
+
+            _listToBeCalculated.ForEach(c => _repository.Add(c));
+        }
+
+        [Fact]
+        public void WhenGenerateIDByList_GetSuccessfulID()
+        {
+            var resultID = Helper.GenerateIDByList(_repository.GetAll());
+            var expectedResult = _listToBeCalculated.Count;
+
+            Assert.Equal(expectedResult, resultID);
+        }
+    }
+}
