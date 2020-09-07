b85e503a7336baebd143451fce75df053ad428de
ddmryrk <d.demiryurek@windowslive.com>
2020-09-07 16:39:43 +0300

    added missing files

---
+++ EcommerceSample/Exceptions/CouponInvalidException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace EcommerceSample.Exceptions
+{
+    public class CouponInvalidException : Exception
+    {
+        public CouponInvalidException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ EcommerceSample/Interfaces/Services/ICouponService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using EcommerceSample.Constants;
+using EcommerceSample.Entities;
+
+namespace EcommerceSample.Interfaces.Services
+{
+    public interface ICouponService
+    {
+        Result Add(Coupon coupon);
+        Coupon GetByID(int id);
+    }
+}
